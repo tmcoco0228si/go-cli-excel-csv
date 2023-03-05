505fb5cf9c2774817697d42083b7873b0cab6253
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-06 00:26:33 +0900

    モック作成　60％ 試運転でExcelファイルを読み込みそのファイル名を使用してCSVファイルを作成・書き込みする処理実装

--- cmd/excelToCsv.go
+++ cmd/excelToCsv.go
@@ -4,8 +4,14 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/spf13/cobra"
 )
 
@@ -16,6 +22,46 @@ var excelToCsvCmd = &cobra.Command{
 	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("excelToCsv called")
+
+		filePath := "サンプル.xlsx"
+		// excelファイル読み込み
+		f, err := excelize.OpenFile(filePath)
+		if err != nil {
+			return err
+		}
+		fmt.Println(f.GetCellValue("Sheet1", "A1"))
+
+		// ファイル名のみを取得する
+		filename := filepath.Base(filePath)
+
+		// .xlsx拡張子を取り除いたファイル名を取得する
+		newFileName := strings.TrimSuffix(filename, filepath.Ext(filename))
+
+		// xlsxファイル名を元にCSVファイル作成
+		csvFile, err := os.Create(newFileName + ".csv")
+		if err != nil {
+			return err
+		}
+		// csvFile.Close()
+
+		// CSV書き込み
+		csvWriter := csv.NewWriter(csvFile)
+		//内部バッファのフラッシュ
+		defer csvWriter.Flush()
+
+		csvWriter.Write([]string{f.GetCellValue("Sheet1", "A1")})
+
+		r := csv.NewReader(csvFile)
+		rows, err := r.ReadAll() // csvを一度に全て読み込む
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// [][]stringなのでループする
+		for _, v := range rows {
+			fmt.Println(v)
+		}
+
 		return nil
 	},
 }
