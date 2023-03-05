c1850a48e97ff511ec05c817c58f6bfdcde0c4b3
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-03-05 23:42:26 +0900

    コメント削除

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,10 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
 	"fmt"
 	"os"
 
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -34,20 +14,23 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-cli-excel-csv",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "コマンドに関する説明 short version",
+	Long:  `コマンドに関する説明 long version`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	// 実際の処理
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		// excelファイル読み込み
+		f, err := excelize.OpenFile("サンプル.xlsx")
+		if err != nil {
+			return err
+		}
+		f.SetCellValue("Sheet1", "A1", 123)
+		fmt.Println(f.GetCellValue("Sheet1", "A1"))
+		return nil
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -58,24 +41,19 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-cli-excel-csv.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// フラグ設定
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
+// 設定ファイルと ENV 変数を読み込む。設定ファイルと ENV 変数を読み込む。
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
+		// コンフィグファイルのパス、名前、拡張子を明示的に定義します
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
+		//$HOME環境変数を返します
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
